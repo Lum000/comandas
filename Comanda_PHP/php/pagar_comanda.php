42740343d31d79c0b29ed4429924ad6eb2730c2e
Lucas <88864398+Lum000@users.noreply.github.com>
2023-02-27 22:28:03 -0300

    commit

---
+++ Comanda_PHP/php/pagar_comanda.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['PAGAR'])) {
+    $productId = $_POST['id'];
+    $people = $_POST['people'];
+    echo $productId;
+    echo $people;
+    
+    // Atualiza o banco de dados para remover
+}  
+?>
